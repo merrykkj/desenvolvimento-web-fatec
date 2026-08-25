07b9c200d661e1bb48b025283b33f4349c913203
Aluno <aluno@IPIRANGA.FATEC>
2026-08-25 17:51:35 -0300

    feat(layout): adicionar estrutura principal de navegação com router-outlet

--- aula-04-html/web04/src/app/app.routes.ts
+++ aula-04-html/web04/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Vitrine } from './vitrine/vitrine'
+import { Cadastro } from './cadastro/cadastro';
+export const routes: Routes = [
+    {path:"", component:Vitrine},
+    {path:"vitrine", component:Vitrine},
+    {path:"cadastro", component:Cadastro}
+];
